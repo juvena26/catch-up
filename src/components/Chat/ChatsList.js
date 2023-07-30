c1c2d437afd4b739cee80c5f0df369d3b366f181
Juvena Ponamma <juvenaponamma@gmail.com>
2023-07-31 08:07:59 +0900

    navigation updates

--- src/components/Chat/ChatsList.js
+++ src/components/Chat/ChatsList.js
@@ -1,41 +1,56 @@
 import React from "react";
 import Nala from "../../images/Nala.jpg";
+import { Link } from "react-router-dom";
+import { BsChevronLeft } from "react-icons/bs";
 const ChatsList = () => {
   return (
     <>
+      <span className="backtochat">
+        {" "}
+        <Link to="/chat">
+          <BsChevronLeft />
+        </Link>
+        Back to Chat
+      </span>
+      {/* <div className="c-bg"> */}
       <div className="c-sidenav">
         <div className="c-list">
-          <div className="c-listitem">
-            <div className="profile-info">
-              <img className="list-pic" src={Nala} alt="" />
-              <span className="list-name">Nala</span>
-            </div>
+          <Link className="link-text" to="/chat">
+            <div className="c-listitem">
+              <div className="profile-info">
+                <img className="list-pic" src={Nala} alt="" />
+                <span className="list-name">Nala</span>
+              </div>
 
-            <div className="message-in-list">
-              <span>Hey there!</span>
+              <div className="message-in-list">
+                <span>Hey there!</span>
+              </div>
             </div>
-          </div>
+          </Link>
+          <Link className="link-text" to="/chat">
+            <div className="c-listitem">
+              <div className="profile-info">
+                <img className="list-pic" src={Nala} alt="" />
+                <span className="list-name">Nala</span>
+              </div>
 
-          <div className="c-listitem">
-            <div className="profile-info">
-              <img className="list-pic" src={Nala} alt="" />
-              <span className="list-name">Nala</span>
+              <div className="message-in-list">
+                <span>Hey there!</span>
+              </div>
             </div>
+          </Link>
+          <Link className="link-text" to="/chat">
+            <div className="c-listitem">
+              <div className="profile-info">
+                <img className="list-pic" src={Nala} alt="" />
+                <span className="list-name">Nala</span>
+              </div>
 
-            <div className="message-in-list">
-              <span>Hey there!</span>
+              <div className="message-in-list">
+                <span>Hey there!</span>
+              </div>
             </div>
-          </div>
-          <div className="c-listitem">
-            <div className="profile-info">
-              <img className="list-pic" src={Nala} alt="" />
-              <span className="list-name">Nala</span>
-            </div>
-
-            <div className="message-in-list">
-              <span>Hey there!</span>
-            </div>
-          </div>
+          </Link>
           <div className="c-listitem">
             <div className="profile-info">
               <img className="list-pic" src={Nala} alt="" />
@@ -68,6 +83,7 @@ const ChatsList = () => {
           </div>
         </div>
       </div>
+      {/* </div> */}
     </>
   );
 };
--- src/components/Chat/ChatsNavbar.js
+++ src/components/Chat/ChatsNavbar.js
@@ -3,8 +3,9 @@ import Nala from "../../images/Nala.jpg";
 import { IoCalendar, IoVideocam } from "react-icons/io5";
 import { BiPhone } from "react-icons/bi";
 import { BsThreeDotsVertical } from "react-icons/bs";
-import { BsChevronLeft } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import { BsChevronLeft } from "react-icons/bs";
+import { IoListCircleOutline } from "react-icons/io5";
 const ChatsNavbar = () => {
   return (
     <div className="c-toolbar">
@@ -12,10 +13,13 @@ const ChatsNavbar = () => {
         <Link to="/">
           <BsChevronLeft />
         </Link>
+        <Link className="only-mobile" to="/chatlist">
+          {" "}
+          <IoListCircleOutline />
+        </Link>
       </div>
-
       <span className="menu-icons">
-        <img src={Nala} alt="" />
+        <img src={Nala} alt="Nala" />
         <span className="profile-name">Nala</span>
       </span>
 
